ad0c5de367c4de9c1ffe309890fc105b75ac6fef
JCRamires <jcramires851@gmail.com>
2015-10-10 01:31:37 -0300

    initial commit

---
+++ javascript/hamming/hamming.js
@@ -0,0 +1,16 @@
+var compute = function(firstStrand, secondStrand) {
+    if (firstStrand.length !== secondStrand.length) {
+        throw new Error('DNA strands must be of equal length.')
+    }
+
+    var hamming = 0
+    for (var i = 0; i < firstStrand.length; i++) {
+        if (firstStrand[i] !== secondStrand[i]) {
+            hamming++
+        }
+    }
+
+    return hamming
+}
+
+module.exports = compute;
---
+++ javascript/rna-transcription/rna_transcription.js
@@ -0,0 +1,33 @@
+var DnaTranscriber = function() {}
+
+DnaTranscriber.prototype.dnaPairs = {
+    c: 'G',
+    g: 'C',
+    a: 'U',
+    t: 'A',
+}
+
+DnaTranscriber.prototype.rnaPairs = {
+    c: 'G',
+    g: 'C',
+    u: 'A',
+    a: 'T',
+}
+
+DnaTranscriber.prototype.toRna = function(strand) {
+    var complement = ''
+    for (var i = 0; i < strand.length; i++) {
+        complement += this.dnaPairs[strand[i].toLowerCase()]
+    }
+    return complement
+}
+
+DnaTranscriber.prototype.toDna = function(strand) {
+    var complement = ''
+    for (var i = 0; i < strand.length; i++) {
+        complement += this.rnaPairs[strand[i].toLowerCase()]
+    }
+    return complement
+}
+
+module.exports = DnaTranscriber;
